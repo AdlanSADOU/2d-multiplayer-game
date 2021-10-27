20c1bd9f39c0ae38260e5acfb612de10f4821382
adlan.sadou <adlsadou@gmail.com>
2021-10-27 18:19:37 +0200

    simple window

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string> // required on windows for std::to_string(...)
+#include <SFML/Graphics.hpp>
 #include "Pig.hpp"
 
 int main(void)
@@ -14,5 +15,20 @@ int main(void)
     Pig peppa(10);
     std::cout << std::to_string(peppa.GetPigSize()) << std::endl;
 
-    return (0);
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
 }
