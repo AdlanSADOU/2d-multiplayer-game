1ad25e89d2c3973fe8852fe12797d7e74a5ff7a1
adlan.sadou <adlsadou@gmail.com>
2021-10-31 10:32:35 +0100

    gameObject first draft

---
+++ engine/GameObject.h
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-NCE-5-1-rtype-adlan.sadou
+** File description:
+** GameObject
+*/
+
+#pragma once
+
+#include <EscCore/Scene.h>
+#include <string>
+
+class GameObject
+{
+private:
+    std::shared_ptr<Scene> _scene;
+    std::string _name;
+    Entity _entity;
+
+public:
+    GameObject(){};
+
+    GameObject(std::shared_ptr<Scene> scene, std::string name)
+    {
+        Create(scene, name);
+    };
+
+    void Create(std::shared_ptr<Scene> scene, std::string name)
+    {
+        _scene = scene;
+        _name = name;
+        _entity = _scene->CreateEntity();
+    }
+
+    template <typename T>
+    void AddComponent()
+    {
+        _scene->AddComponent(_entity, T{});
+    };
+
+    void RemoveComponent()
+    {
+        
+    }
+
+    template <typename T>
+    T GetComponent()
+    {
+       return _scene->GetComponent<T>(_entity);
+    }
+
+    Entity GetEntity()
+    {
+        return _entity;
+    }
+};
