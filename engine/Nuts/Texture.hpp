6fdc03d5c8adeb0bcbf35eed78661e3e8c657cb3
Marius Contoli <marius.contoli-legay@epitech.eu>
2021-11-21 16:42:07 +0100

    Add comments about the audio, clock, color, input, networking, texture, vect classes.

--- engine/Nuts/Texture.hpp
+++ engine/Nuts/Texture.hpp
@@ -12,25 +12,54 @@
 
 namespace nuts
 {
+    /**
+     * @brief Image living on the graphics card that can be used for drawing.
+     * 
+     */
     class Texture
     {
         public:
 
+            /**
+             * @brief Construct a new Texture object
+             * 
+             */
             Texture()
             {
 
             }
 
+            /**
+             * @brief Destroy the Texture object
+             * 
+             */
             ~Texture()
             {
                 _texture.~Texture();
             }
 
+            /**
+             * @brief Load the texture from a file on disk.
+             * This function is a shortcut for the following code:
+             * sf::Image image;
+             * image.loadFromFile(filename);
+             * texture.loadFromImage(image, area);
+             * The area argument can be used to load only a sub-rectangle of the whole image. If you want the entire image then leave the default value (which is an empty IntRect). If the area rectangle crosses the bounds of the image, it is adjusted to fit the image size.
+             * The maximum size for a texture depends on the graphics driver and can be retrieved with the getMaximumSize function.
+             * If this function fails, the texture is left unchanged.
+             * @param path Path of the image file to load
+             * @return True if loading was successful
+             */
             bool LoadFromFile(const std::string &path)
             {
                 return (_texture.loadFromFile(path));
             }
-
+            
+            /**
+             * @brief Return the current texture.
+             * 
+             * @return const sf::Texture& 
+             */
             const sf::Texture &GetTexture()
             {
                 return (this->_texture);
--- engine/Nuts/Vect.hpp
+++ engine/Nuts/Vect.hpp
@@ -9,24 +9,40 @@
 
 namespace nuts
 {
+    /**
+     * @brief Utility struct for manipulating 2-dimensional vectors.
+     * Coordinates represented by int values.
+     */
     typedef struct
     {
         int x;
         int y;
     } Vector2i;
 
+    /**
+     * @brief Utility struct for manipulating 2-dimensional vectors.
+     * Coordinates represented by unsigned int values.
+     */
     typedef struct
     {
         unsigned int x;
         unsigned int y;
     } Vector2u;
 
+    /**
+     * @brief Utility struct for manipulating 2-dimensional vectors.
+     * Coordinates represented by float values.
+     */
     typedef struct
     {
         float x;
         float y;
     } Vector2f;
 
+    /**
+     * @brief Utility struct for manipulating 3-dimensional vectors.
+     * Coordinates represented by int values.
+     */
     typedef struct
     {
         int x;
@@ -34,6 +50,10 @@ namespace nuts
         int z;
     } Vector3i;
 
+    /**
+     * @brief Utility struct for manipulating 3-dimensional vectors.
+     * Coordinates represented by float values.
+     */
     typedef struct
     {
         float x;
